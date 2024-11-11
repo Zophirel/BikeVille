a39bbe239b7678b42da3e4d78ab803d4444f287d
Francesco <sworld.design@gmail.com>
2024-11-11 12:32:02 +0100

    first initialization with db first scaffolding

---
+++ DbContext/BikeVilleCustomersContext.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BikeVille;
+
+public partial class BikeVilleCustomersContext : DbContext
+{
+    public BikeVilleCustomersContext()
+    {
+    }
+
+    public BikeVilleCustomersContext(DbContextOptions<BikeVilleCustomersContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Address> Addresses { get; set; }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<CustomerAddress> CustomerAddresses { get; set; }
+
+    public virtual DbSet<SalesOrderDetail> SalesOrderDetails { get; set; }
+
+    public virtual DbSet<SalesOrderHeader> SalesOrderHeaders { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=FRA-PC\\SQLEXPRESS;Initial Catalog=BikeVilleCustomers;Integrated Security=True;Connect Timeout=30;Encrypt=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Address>(entity =>
+        {
+            entity.ToTable("Address");
+
+            entity.Property(e => e.AddressId).HasColumnName("AddressID");
+            entity.Property(e => e.AddressLine1)
+                .HasMaxLength(60)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.AddressLine2)
+                .HasMaxLength(60)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.City)
+                .HasMaxLength(30)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.CountryRegion)
+                .HasMaxLength(50)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.PostalCode)
+                .HasMaxLength(15)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.Rowguid).HasColumnName("rowguid");
+            entity.Property(e => e.StateProvince)
+                .HasMaxLength(50)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+        });
+
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.ToTable("Customer");
+
+            entity.Property(e => e.CustomerId).HasColumnName("CustomerID");
+            entity.Property(e => e.CompanyName)
+                .HasMaxLength(128)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.EmailAddress)
+                .HasMaxLength(50)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(50)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.MiddleName)
+                .HasMaxLength(50)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.PasswordHash)
+                .HasMaxLength(128)
+                .IsUnicode(false)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.PasswordSalt)
+                .HasMaxLength(10)
+                .IsUnicode(false)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.Phone)
+                .HasMaxLength(25)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.Rowguid).HasColumnName("rowguid");
+            entity.Property(e => e.SalesPerson)
+                .HasMaxLength(256)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.Suffix)
+                .HasMaxLength(10)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.Title)
+                .HasMaxLength(8)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+        });
+
+        modelBuilder.Entity<CustomerAddress>(entity =>
+        {
+            entity.HasKey(e => new { e.CustomerId, e.AddressId });
+
+            entity.ToTable("CustomerAddress");
+
+            entity.Property(e => e.CustomerId).HasColumnName("CustomerID");
+            entity.Property(e => e.AddressId).HasColumnName("AddressID");
+            entity.Property(e => e.AddressType)
+                .HasMaxLength(50)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.Rowguid).HasColumnName("rowguid");
+
+            entity.HasOne(d => d.Address).WithMany(p => p.CustomerAddresses)
+                .HasForeignKey(d => d.AddressId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_CustomerAddress_Address");
+
+            entity.HasOne(d => d.Customer).WithMany(p => p.CustomerAddresses)
+                .HasForeignKey(d => d.CustomerId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_CustomerAddress_Customer");
+        });
+
+        modelBuilder.Entity<SalesOrderDetail>(entity =>
+        {
+            entity.HasKey(e => new { e.SalesOrderId, e.SalesOrderDetailId });
+
+            entity.ToTable("SalesOrderDetail", tb => tb.HasTrigger("trg_CheckProductID"));
+
+            entity.Property(e => e.SalesOrderId)
+                .ValueGeneratedOnAdd()
+                .HasColumnName("SalesOrderID");
+            entity.Property(e => e.SalesOrderDetailId).HasColumnName("SalesOrderDetailID");
+            entity.Property(e => e.LineTotal).HasColumnType("numeric(38, 6)");
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.ProductId).HasColumnName("ProductID");
+            entity.Property(e => e.Rowguid).HasColumnName("rowguid");
+            entity.Property(e => e.UnitPrice).HasColumnType("money");
+            entity.Property(e => e.UnitPriceDiscount).HasColumnType("money");
+
+            entity.HasOne(d => d.SalesOrder).WithMany(p => p.SalesOrderDetails)
+                .HasForeignKey(d => d.SalesOrderId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_SalesOrderDetail_SalesOrderHeader");
+        });
+
+        modelBuilder.Entity<SalesOrderHeader>(entity =>
+        {
+            entity.HasKey(e => e.SalesOrderId);
+
+            entity.ToTable("SalesOrderHeader");
+
+            entity.Property(e => e.SalesOrderId)
+                .ValueGeneratedNever()
+                .HasColumnName("SalesOrderID");
+            entity.Property(e => e.AccountNumber)
+                .HasMaxLength(15)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.BillToAddressId).HasColumnName("BillToAddressID");
+            entity.Property(e => e.Comment).UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.CreditCardApprovalCode)
+                .HasMaxLength(15)
+                .IsUnicode(false)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.CustomerId).HasColumnName("CustomerID");
+            entity.Property(e => e.DueDate).HasColumnType("datetime");
+            entity.Property(e => e.Freight).HasColumnType("money");
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.OrderDate).HasColumnType("datetime");
+            entity.Property(e => e.PurchaseOrderNumber)
+                .HasMaxLength(25)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.Rowguid).HasColumnName("rowguid");
+            entity.Property(e => e.SalesOrderNumber)
+                .HasMaxLength(25)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.ShipDate).HasColumnType("datetime");
+            entity.Property(e => e.ShipMethod)
+                .HasMaxLength(50)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.ShipToAddressId).HasColumnName("ShipToAddressID");
+            entity.Property(e => e.SubTotal).HasColumnType("money");
+            entity.Property(e => e.TaxAmt).HasColumnType("money");
+            entity.Property(e => e.TotalDue).HasColumnType("money");
+
+            entity.HasOne(d => d.BillToAddress).WithMany(p => p.SalesOrderHeaderBillToAddresses)
+                .HasForeignKey(d => d.BillToAddressId)
+                .HasConstraintName("FK_BillAddress");
+
+            entity.HasOne(d => d.Customer).WithMany(p => p.SalesOrderHeaders)
+                .HasForeignKey(d => d.CustomerId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_CustomerId");
+
+            entity.HasOne(d => d.ShipToAddress).WithMany(p => p.SalesOrderHeaderShipToAddresses)
+                .HasForeignKey(d => d.ShipToAddressId)
+                .HasConstraintName("FK_ShipAddress");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DbContext/BikeVilleProductsContext.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BikeVille;
+
+public partial class BikeVilleProductsContext : DbContext
+{
+    public BikeVilleProductsContext()
+    {
+    }
+
+    public BikeVilleProductsContext(DbContextOptions<BikeVilleProductsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<ProductCategory> ProductCategories { get; set; }
+
+    public virtual DbSet<ProductDescription> ProductDescriptions { get; set; }
+
+    public virtual DbSet<ProductModel> ProductModels { get; set; }
+
+    public virtual DbSet<ProductModelProductDescription> ProductModelProductDescriptions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=FRA-PC\\SQLEXPRESS;Initial Catalog=BikeVilleProducts;Integrated Security=True;Connect Timeout=30;Encrypt=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.ToTable("Product");
+
+            entity.Property(e => e.ProductId).HasColumnName("ProductID");
+            entity.Property(e => e.Color)
+                .HasMaxLength(15)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.DiscontinuedDate).HasColumnType("datetime");
+            entity.Property(e => e.ListPrice).HasColumnType("money");
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.ProductCategoryId).HasColumnName("ProductCategoryID");
+            entity.Property(e => e.ProductModelId).HasColumnName("ProductModelID");
+            entity.Property(e => e.ProductNumber)
+                .HasMaxLength(25)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.Rowguid).HasColumnName("rowguid");
+            entity.Property(e => e.SellEndDate).HasColumnType("datetime");
+            entity.Property(e => e.SellStartDate).HasColumnType("datetime");
+            entity.Property(e => e.Size)
+                .HasMaxLength(5)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.StandardCost).HasColumnType("money");
+            entity.Property(e => e.ThumbnailPhotoFileName)
+                .HasMaxLength(50)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.Weight).HasColumnType("decimal(8, 2)");
+
+            entity.HasOne(d => d.ProductCategory).WithMany(p => p.Products)
+                .HasForeignKey(d => d.ProductCategoryId)
+                .HasConstraintName("FK_Product_Category");
+
+            entity.HasOne(d => d.ProductModel).WithMany(p => p.Products)
+                .HasForeignKey(d => d.ProductModelId)
+                .HasConstraintName("FK_Product_ProductModel");
+        });
+
+        modelBuilder.Entity<ProductCategory>(entity =>
+        {
+            entity.ToTable("ProductCategory");
+
+            entity.Property(e => e.ProductCategoryId).HasColumnName("ProductCategoryID");
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.ParentProductCategoryId).HasColumnName("ParentProductCategoryID");
+            entity.Property(e => e.Rowguid).HasColumnName("rowguid");
+
+            entity.HasOne(d => d.ParentProductCategory).WithMany(p => p.InverseParentProductCategory)
+                .HasForeignKey(d => d.ParentProductCategoryId)
+                .HasConstraintName("FK_ProductCategory_ParentProductCategory");
+        });
+
+        modelBuilder.Entity<ProductDescription>(entity =>
+        {
+            entity.ToTable("ProductDescription");
+
+            entity.Property(e => e.ProductDescriptionId).HasColumnName("ProductDescriptionID");
+            entity.Property(e => e.Description)
+                .HasMaxLength(400)
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.Rowguid).HasColumnName("rowguid");
+        });
+
+        modelBuilder.Entity<ProductModel>(entity =>
+        {
+            entity.HasKey(e => e.ProductModelId).HasName("PK__ProductM__DB7B7E9B94DE0921");
+
+            entity.ToTable("ProductModel");
+
+            entity.Property(e => e.ProductModelId)
+                .ValueGeneratedNever()
+                .HasColumnName("ProductModelID");
+            entity.Property(e => e.CatalogDescription).HasColumnType("xml");
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.Name).HasMaxLength(50);
+            entity.Property(e => e.Rowguid).HasColumnName("rowguid");
+        });
+
+        modelBuilder.Entity<ProductModelProductDescription>(entity =>
+        {
+            entity.HasKey(e => new { e.ProductModelId, e.ProductDescriptionId, e.Culture });
+
+            entity.ToTable("ProductModelProductDescription");
+
+            entity.Property(e => e.ProductModelId).HasColumnName("ProductModelID");
+            entity.Property(e => e.ProductDescriptionId).HasColumnName("ProductDescriptionID");
+            entity.Property(e => e.Culture)
+                .HasMaxLength(6)
+                .IsFixedLength()
+                .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.Rowguid).HasColumnName("rowguid");
+
+            entity.HasOne(d => d.ProductDescription).WithMany(p => p.ProductModelProductDescriptions)
+                .HasForeignKey(d => d.ProductDescriptionId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_ProductModelProductDescription_Description");
+
+            entity.HasOne(d => d.ProductModel).WithMany(p => p.ProductModelProductDescriptions)
+                .HasForeignKey(d => d.ProductModelId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_ProductModelProductDescription_Model");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
